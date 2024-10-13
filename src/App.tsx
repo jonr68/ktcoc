6dee8b7a4c3e9934d3af2b22f8a111b4b7ec2e9b
JonR205 <gohan205@gmail.com>
2024-10-13 18:18:19 -0500

    created NPO type and started on opbjects

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrawlervTrouper } from "./data/NPOs";
+
+// const BrawlerHeavy = BrawlervTrouper
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h1>Name: {BrawlervTrouper.name}</h1>
+      <h2>Size:{BrawlervTrouper.level}</h2>
+      <p>APL: {BrawlervTrouper.apl}</p>
+      <p>Move:{BrawlervTrouper.move}</p>
+      <p>Save:{BrawlervTrouper.save}</p>
+      <p>Wounds: {BrawlervTrouper.wounds}</p>
+      
+    </div>
+  );
 }
 
 export default App;
---
+++ src/data/NPOs.tsx
@@ -0,0 +1,35 @@
+export interface NPO {
+  name: string;
+  level: string;
+  apl: number;
+  move: number;
+  save: number;
+  wounds: number;
+}
+
+export const BrawlervTrouper: NPO = {
+  name: "Brawler",
+  level: "Trooper",
+  apl: 2,
+  move: 6,
+  save: 5,
+  wounds: 7,
+};
+
+// export const BrawlervTough: NPO = {
+//   name: "Brawler";
+//   level: "Tough";
+//   apl: 2;
+//   move: 5;
+//   save: 4;
+//   wounds: 10;
+// }
+
+// export interface BrawlervHeavy  {
+//     name: "Brawler";
+//     level: "Heavy";
+//     apl: 3;
+//     move: 6;
+//     save: 3;
+//     wounds: 14;
+//   }
