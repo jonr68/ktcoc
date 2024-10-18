8306841347b9a9cb1c2ac9ad4a7d293878ac3c93
JonR205 <gohan205@gmail.com>
2024-10-18 02:17:19 -0500

    Added missing import and started on card design

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import "bulma/css/bulma.css"
 import { MarksmanTrooper } from "./data/NPOs";
 import NPOCard from "./card/NPOCards";
 
--- src/card/NPOCards.tsx
+++ src/card/NPOCards.tsx
@@ -2,24 +2,30 @@ import { MarksmanTrooper } from "../data/NPOs";
 
 const NPOCard = () => {
   return (
-    <div className="card">
-      <header className="card-header">
-        <p className="card-header-title">Name: {MarksmanTrooper.name}</p>
-        {/* <p>Name: {MarksmanTrooper.name}<p>
-        <h3>Size:{MarksmanTrooper.level}</h3>
-        <p>APL: {MarksmanTrooper.apl}</p>
-        <p>Move:{MarksmanTrooper.move}"</p>
-        <p>Save:{MarksmanTrooper.save}+</p>
-        <p>Wounds: {MarksmanTrooper.wounds}</p>
-        <ul>
-          {MarksmanTrooper.weapon.map((item, index) => (
-            <li key={index}>
-              {item.name} ATK: {item.atk} HIT: {item.hit} DMG: {item.dmg}
-              WR: {item.wr}
-            </li> */}
-        {/* ))} */}
-        {/* </ul> */}
-      </header>
+    <div className="column is-one-quarter">
+      <div className="card has-background-light has-text-black">
+        <header className="card-header">
+          <p className="card-header-title has-text-black">
+            Class: {MarksmanTrooper.name}
+          </p>
+        </header>
+        <div className="card-content">
+          <h3>Type: {MarksmanTrooper.level}</h3>
+          <p>APL: {MarksmanTrooper.apl}</p>
+          <p>Move:{MarksmanTrooper.move}"</p>
+          <p>Save:{MarksmanTrooper.save}+</p>
+          <p>Wounds: {MarksmanTrooper.wounds}</p>
+          <br />
+          <h3>Weapons: </h3>
+          <ul>
+            {MarksmanTrooper.weapon.map((item, index) => (
+              <li key={index}>
+                {item.name}    ATK:{item.atk}    HIT:{item.hit}   DMG:{item.dmg}    WR:{item.wr}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
