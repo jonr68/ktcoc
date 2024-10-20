882ebeb9d8a5860c308354793b77dfa24741c872
JonR205 <gohan205@gmail.com>
2024-10-19 21:48:54 -0500

    Finished basic card design

--- src/card/NPOCards.tsx
+++ src/card/NPOCards.tsx
@@ -1,4 +1,4 @@
-import { MarksmanTrooper } from "../data/NPOs";
+import { BrawlervHeavy } from "../data/NPOs";
 
 const NPOCard = () => {
   return (
@@ -6,24 +6,27 @@ const NPOCard = () => {
       <div className="card has-background-light has-text-black">
         <header className="card-header">
           <p className="card-header-title has-text-black">
-            Class: {MarksmanTrooper.name}
+            Class: {BrawlervHeavy.name}
           </p>
         </header>
         <div className="card-content">
-          <h3>Type: {MarksmanTrooper.level}</h3>
-          <p>APL: {MarksmanTrooper.apl}</p>
-          <p>Move:{MarksmanTrooper.move}"</p>
-          <p>Save:{MarksmanTrooper.save}+</p>
-          <p>Wounds: {MarksmanTrooper.wounds}</p>
+          <h3>Type: {BrawlervHeavy.level}</h3>
+          <p>APL: {BrawlervHeavy.apl}</p>
+          <p>Move:{BrawlervHeavy.move}"</p>
+          <p>Save:{BrawlervHeavy.save}+</p>
+          <p>Wounds: {BrawlervHeavy.wounds}</p>
           <br />
           <h3>Weapons: </h3>
-          <ul>
-            {MarksmanTrooper.weapon.map((item, index) => (
+          <div className="grid">
+            {BrawlervHeavy.weapon.map((item, index) => (
               <li key={index}>
-                {item.name}    ATK:{item.atk}    HIT:{item.hit}   DMG:{item.dmg}    WR:{item.wr}
+                {item.name} <div className="cell">ATK: {item.atk} </div>{" "}
+                <div className="cell"> HIT:{item.hit}</div>{" "}
+                <div className="cell"> DMG:{item.dmg}</div>{" "}
+                <div className="cell"> WR:{item.wr}</div>
               </li>
             ))}
-          </ul>
+          </div>
         </div>
       </div>
     </div>
