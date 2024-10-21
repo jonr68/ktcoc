a3ca4440bc8ec54ccbbb08e6f2fa31e2bf2b11fc
JonR205 <gohan205@gmail.com>
2024-10-21 14:23:10 -0500

    Is addingd NPOs to crew and adding wounds

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -26,17 +26,34 @@ const Dashboard = () => {
 
   const addToNPOCrew = (newNPO: NPO) => {
     setNPOCrew([...NPOCrew, newNPO]);
-    console.log(NPOCrew);
   };
+
+  let totalWounds = NPOCrew.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue.wounds;
+  }, 0);
 
   return (
     <>
+      <section>
+        <div> Selected NPOs </div>
+        <ol>
+          {NPOCrew.map((crew) => (
+            <li key={crew.name}>
+              {crew.name} {crew.level}
+            </li>
+          ))}
+        </ol>
+        <div> Total Wounds: {totalWounds}</div>
+      </section>
       <div className="fixed-grid has-3-cols">
         <div className="grid">
           {NPOList?.map((npo, index) => {
             return (
               <>
                 <div key={index} className="cell">
+                  <button key={index} onClick={() => addToNPOCrew(npo)}>
+                    Add {npo.name} {npo.level} to Crew
+                  </button>
                   <NPOCard npo={npo} />
                 </div>
               </>
